ede0691c6a51f26d76a2df57a1a8774a92af7116
psanket41 <37711191+psanket41@users.noreply.github.com>
2018-12-20 08:00:57 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,19 @@
+int max_value=0;
+int temp_time=0;
+
+int hackflipmart(){
+temp_time=time;
+for(int i=0;i<4;i++)
+{
+for(iint j=0;j<2;j++)
+{
+temp_time=temp_time-j[0];
+
+if(temp_time-j[0]>)
+{
+max_value=j[1];
+}
+}
+}
+return max_value;
+}
